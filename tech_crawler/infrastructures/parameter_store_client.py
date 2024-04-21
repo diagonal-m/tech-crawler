ea64085a89da1271af8c1840b5a3acfa0a8e9fc6
佐藤太一 <3101taichi@gmail.com>
2024-04-21 11:18:20 +0900

    parameter_store_client実装

--- tech_crawler/infrastructures/parameter_store_client.py
+++ tech_crawler/infrastructures/parameter_store_client.py
@@ -1,8 +1,17 @@
 from interfaces.parameter_store import ParameterStore
+import boto3
+
 
 class ParameterStoreClient(ParameterStore):
     def __init__(self):
-        pass
+        self.params = self.params()
 
     def params(self):
-        return {}
+        return {
+            'crawling_master_file_name': 'crawling_master.csv',
+            'crawling_master_bucket_name': self._get_parameter('crawling-master-bucket-name')
+        }
+
+    def _get_parameter(self, name: str) -> str:
+        ssm = boto3.client('ssm')
+        return ssm.get_parameter(Name=name, WithDecryption=True)['Parameter']['Value']
