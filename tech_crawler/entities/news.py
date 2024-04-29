5b15270bba0596685c635164355504959e5ac4bb
佐藤太一 <3101taichi@gmail.com>
2024-04-29 16:33:42 +0900

    微修正

--- tech_crawler/entities/news.py
+++ tech_crawler/entities/news.py
@@ -2,6 +2,6 @@ from urllib.parse import urljoin
 
 class News():
   def __init__(self, site_name: str, title: str, site_url: str, href: str):
-    self.site_name = site_name
-    self.title = title
+    self.site_name = site_name.strip()
+    self.title = title.strip()
     self.url = urljoin(site_url, href)
